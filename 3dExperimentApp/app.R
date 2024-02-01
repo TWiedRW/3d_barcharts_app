840c305d994e301efa000286df36133474d942c1
TWiedRW <61435221+TWiedRW@users.noreply.github.com>
2024-02-01 14:55:24 -0600

    Save while it's working

--- 3dExperimentApp/app.R
+++ 3dExperimentApp/app.R
@@ -5,6 +5,8 @@ source('R/plot-code.R')
 graphData <- read.csv('Data/graphData.csv')
 printedKitInfo <- read.csv('Data/printedKitInfo.csv')
 practiceData <- read.csv('Data/practiceData.csv')
+source('R/create_lineup.R')
+source('R/generate_code.R')
 
 
 ##### UI Pages #####
@@ -15,19 +17,19 @@ informed_consent <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       radioButtons("statStudent", "Are you currently enrolled in STAT 218?",
-                   c("Yes" = "yes",
-                     "No" = "no"), selected = ''),
+                   c("Yes" = TRUE,
+                     "No" = FALSE), selected = ''),
       uiOutput("informed_consent"),
       uiOutput("informed_consent_button")
     
     ),
     mainPanel(
       conditionalPanel(
-        condition = "input.statStudent == 'yes'",
+        condition = "input.statStudent == 'TRUE'",
         includeHTML("Informed Consent/graphics-consent-218.html")
       ),
       conditionalPanel(
-        condition = "input.statStudent == 'no'",
+        condition = "input.statStudent == 'FALSE'",
         includeHTML("Informed Consent/graphics-consent-dept.html")
       )
     )
@@ -78,13 +80,10 @@ instructions <- fluidPage(
         'In this survey, you will see a series of graphs. Each graph will have 10 bars; two of the bars will be identified with a circle and a triangle.',
         'First, we will ask you to identify which bar (circle or triangle) is smaller.',
         'Then, we will ask you to estimate the size of the smaller bar if the larger bar is 100 units tall.'),
-      p('Before you start, please enter your kit number in the box below.',
-        'This number is located on the index card inside the bag of 3D-printed graphs.',
-        'If you do not have a bag of 3D-printed graphs, please check the "Online Participant" checkbox to verify that you are not an in-person participant.'),
+      p('On the next page, you will be presented with a series of three practice graphs.',
+        'Your responses will not be saved, but you will be able to see the correct answers after you submit your responses.'),
+      actionButton('startPractice', 'Start Practice')
       
-      selectInput('kitNumber', 'Kit Number', choices = c('',1:21), selected = 'Oh hello'),
-      checkboxInput('onlineParticipant', 'Online Participant: Please only check this box if you do not have access to the 3D printed bar charts.', value = FALSE),
-      uiOutput('instructions')
       
 
 )
@@ -99,7 +98,7 @@ practice <- fluidPage(
   useShinyjs(),
   sidebarLayout(
     sidebarPanel(
-      h2('Practice'),
+      uiOutput('practiceGraphTitle'),
       radioButtons('practiceGraphs', 'Which marked bar is smaller?',
                    c('Circle', 'Triangle'), selected = ''),
       sliderInput('practiceEstimate', 'If the larger marked bar is 100 units tall, how tall is the smaller marked bar?',
@@ -114,16 +113,70 @@ practice <- fluidPage(
   )
 )
 
+##### Setup #####
+
+setup <- fluidPage(
+  h2('Welcome to the Graphics Experiment'),
+  p('Before you start, please enter your kit number in the box below.',
+    'This number is located on the index card inside the bag of 3D-printed graphs.',
+    'If you do not have a bag of 3D-printed graphs, please check the "Online Participant" checkbox to verify that you are not an in-person participant.'),
+  selectInput('kitNumber', 'Kit Number', choices = c('',1:21), selected = ''),
+  checkboxInput('onlineParticipant', 'Online Participant: Please only check this box if you do not have access to the 3D printed bar charts.', value = FALSE),
+  uiOutput('setup')
+)
+
+
 ##### Experiment page #####
 
 experiment <- fluidPage(
-  h2('Experiment'),
-  p('The experiment is about to begin.'),
-  actionButton('startExperiment', 'Start Experiment')
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
+  ),
+  useShinyjs(),
+  sidebarLayout(
+    sidebarPanel(
+      uiOutput('expGraphTitle'),
+      radioButtons('expGraphs', 'Which marked bar is smaller?',
+                   c('Circle', 'Triangle'), selected = ''),
+      sliderInput('expEstimate', 'If the larger marked bar is 100 units tall, how tall is the smaller marked bar?',
+                  min = 0, max = 100, value = 50, step = 0.1, ticks = F),
+      actionButton('submitExp', 'Submit')
+      
+    ),
+    mainPanel(
+      # uiOutput('expGraphUI')
+    )
+  )
 )
 
 
 
+
+##### Exit #####
+exit <- fluidPage(
+  h2('Thank you'),
+  conditionalPanel(
+    condition = "input.consent == 'TRUE'",
+    p('Thank you for participating in our experiment.',
+      'Your response had been submitted.',
+      'Please return the graphs to the kit bag and close out of this page.'),
+  ),
+  
+  
+  conditionalPanel(
+    condition = "input.consent == 'FALSE'",
+    p('Thank you for participating in our experiment.',
+      'Please return the graphs to the kit bag and close out of this page.'),
+  ),
+  conditionalPanel(
+    condition = "input.statStudent == 'TRUE'",
+    h4('Completion code'),
+    textOutput('completionCode'),
+    helpText("Save this code and submit it to Canvas to complete your Stat 218 assignment. You will not have access to this code when you close the app.")
+  ),
+  p(),
+  actionButton('reset', 'New Submission')
+)
 
 
 
@@ -136,7 +189,9 @@ ui <- navbarPage(
   tabPanel("Demographics", demographic),
   tabPanel("Instructions", instructions),
   tabPanel("Practice", practice),
+  tabPanel("Setup", setup),
   tabPanel("Experiment", experiment),
+  tabPanel("Finishing Up", exit),
   collapsible = TRUE
 )
 
@@ -150,16 +205,19 @@ server <- function(input, output, session) {
     practice_state = 'watch',
     practice_data = NULL,
     practice_data_solution = NULL,
-    practice_data_estimate = NULL
+    practice_data_estimate = NULL,
+    exp_kit = NULL,
+    exp_lineup = NULL,
+    exp_trial = NULL,
+    exp_trial_data = NULL,
+    graphTitle = NULL
   )
-  
-  
-  
-  
   
   output$sessionInfo <- renderText({
     as.character(session$token)
   })
+  
+  
   
   ##### Informed Consent Logic #####
   
@@ -176,26 +234,21 @@ server <- function(input, output, session) {
     list(actionButton("submitConsent", "Next page"))
 
   })
-  
-  
-  
-  
+
   observeEvent(input$submitConsent, {
     
     #Move from informed consent to demographic page
     updateNavbarPage(session, inputId = "navbar", "Demographics")
-    
-    
-      
+
   })
 
   
   ##### Demographics Logic #####
   
   output$demographic <- renderUI({
-    validate(need(input$age != "", 'Please enter your age to continue'))
-    validate(need(input$gender != "", 'Please enter your gender to continue'))
-    validate(need(input$education != "", 'Please enter your education to continue'))
+    validate(need(input$age != "", 'Please select your age to continue'))
+    validate(need(input$gender != "", 'Please select your gender to continue'))
+    validate(need(input$education != "", 'Please select your education to continue'))
     validate(need(input$subjectZoo != "", 'Please enter your favorite zoo animal to continue'))
     actionButton("toInstructions", "Next Page")
   })
@@ -207,23 +260,6 @@ server <- function(input, output, session) {
   ##### Instructions Logic #####
   
   #Update kit value to 'Online' if online participant is checked
-  observeEvent(input$onlineParticipant, {
-    if(input$onlineParticipant == TRUE){
-      updateSelectInput(session, "kitNumber", choices = c('Online'))
-    } else {
-      updateSelectInput(session, "kitNumber", choices = c('',1:21),
-                        selected = )
-    }
-  })
-  
-  output$instructions <- renderUI({
-    validate(need(input$kitNumber %in% c(1:21) | input$onlineParticipant == T, 'Please enter your kit number to continue'))
-    list(p('On the next page, you will be presented with a series of three practice graphs.',
-           'Your responses will not be saved, but you will be able to see the correct answers after you submit your responses.'),
-      actionButton("startPractice", "Start Practice"))
-  })
-  
-  
   observeEvent(input$startPractice, {
     updateNavbarPage(session, inputId = "navbar", "Practice")
   })
@@ -276,32 +312,8 @@ server <- function(input, output, session) {
   
   # Render plot
   output$practiceGraphUI <- renderUI({
-    list(plotOutput('practiceGraph', width = '13cm', height = '9.5cm'))
+    list(plotOutput('practiceGraph', width = '16.175cm', height = '9.5cm'))
   })
-  
-  
-  
-
-
-  #
-  # observeEvent(input$submitPractice, {
-  #   if(trial_data$practice_trial == 3 & trial_data$practice_state == 'learn'){
-  #     updateNavbarPage(session, inputId = "navbar", "Demographics")
-  #     trial_data$practice_trial <- 1
-  #     
-  #   } else{
-  #     trial_data$practice_trial <- trial_data$practice_trial + 1
-  #     shinyjs::disable('practiceEstimate')
-  #     shinyjs::hide('submitPractice')
-  #     trial_data$practice_state <- 'learn'
-  #   }
-  # })
-  # 
-  # observeEvent(input$practiceNext, {
-  #   
-  # })
-  
-  
   
   #Practice solution slider
   output$practiceSolutions <- renderUI({
@@ -313,7 +325,10 @@ server <- function(input, output, session) {
     }
   })
   
-
+  #Practice graph title
+  output$practiceGraphTitle <- renderUI({
+    list(h2(paste('Graph', as.character(trial_data$practice_trial), 'of 3')))
+  })
   
   
   
@@ -327,18 +342,81 @@ server <- function(input, output, session) {
   #Button to continue to next practice graph
   observeEvent(input$practiceNext, {
     if(trial_data$practice_trial == 3 & trial_data$practice_state == 'learn'){
-      updateNavbarPage(session, inputId = "navbar", "Experiment")
       trial_data$practice_trial <- 1
-      
+      updateNavbarPage(session, inputId = "navbar", "Setup")
     } else{
       trial_data$practice_trial <- trial_data$practice_trial + 1
     }
     
     trial_data$practice_state <- 'watch'
     shinyjs::show('submitPractice')
+    updateSliderInput(session, "practiceEstimate", value = 50)
   })
   
   
+  ##### Setup Logic #####
+  
+  #Update kit value to 'Online' if online participant is checked
+  observeEvent(input$onlineParticipant, {
+    if(input$onlineParticipant == TRUE){
+      updateSelectInput(session, "kitNumber", choices = c('Online'))
+    } else {
+      updateSelectInput(session, "kitNumber", choices = c('',1:21),
+                        selected = )
+    }
+  })
+  
+  output$setup <- renderUI({
+    validate(need(input$kitNumber %in% c(1:21) | input$onlineParticipant == T, 'Please enter your kit number to continue'))
+    list(
+      actionButton("beginExp", "Begin Experiment"))
+  })
+  
+  observeEvent(input$beginExp, {
+    trial_data$exp_kit <- input$kitNumber
+    trial_data$exp_lineup <- create_lineup(trial_data$exp_kit)
+    trial_data$exp_trial <- 1
+    updateNavbarPage(session, inputId = "navbar", "Experiment")
+  })
+  
+  ##### Experiment Logic #####
+  
+  output$expGraphTitle <- renderUI({
+    trial_data$graphTitle <- paste('Graph', as.character(trial_data$exp_trial), 
+                        'of', as.character(nrow(trial_data$exp_lineup)))
+    list(h2(trial_data$graphTitle))
+  })
+  
+  # Button to submit experiment estimate, updates slide if 
+  observeEvent(input$submitExp, {
+    message(paste('Participant completed experiment graph', trial_data$exp_trial))
+    trial_data$exp_trial <- trial_data$exp_trial + 1
+    updateSliderInput(session, "expEstimate", value = 50)
+    if(trial_data$exp_trial > nrow(trial_data$exp_lineup)){
+      updateNavbarPage(session, inputId = "navbar", "Finishing Up")
+      trial_data$exp_trial <- NULL
+    }
+  })
+  
+  
+  
+  
+  #Display experiment plot
+  output$expGraphDisplay <- renderPlot({
+    trial_data$
+    # Bar2D(trial_data$exp_lineup, shape_order = 1)
+  })
+  
+  
+  ##### Exit Logic #####
+  
+  output$completionCode <- renderText({
+    generate_code(NULL)
+  })
+  
+  observeEvent(input$reset, {
+    shinyjs::refresh()
+  })
   
   
 }
