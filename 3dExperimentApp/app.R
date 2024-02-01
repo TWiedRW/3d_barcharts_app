6150ab8c11459a13a1ea810d424d043b4e908d19
TWiedRW <61435221+TWiedRW@users.noreply.github.com>
2024-01-31 23:06:22 -0600

    Instructions UI

--- 3dExperimentApp/app.R
+++ 3dExperimentApp/app.R
@@ -64,10 +64,32 @@ demographic <- fluidPage(
 )
 
 
-##### Practice page #####
+##### Instructions #####
+
+instructions <- fluidPage(
+
+      h2('Instructions'),
+      p('Thank you for participating in our experiment on perceptural judgments in different graphical mediums.',
+        'In this survey, you will see a series of graphs. Each graph will have 10 bars; two of the bars will be identified with a circle and a triangle.',
+        'First, we will ask you to identify which bar (circle or triangle) is smaller.',
+        'Then, we will ask you to estimate the size of the smaller bar if the larger bar is 100 units tall.'),
+      p('Before you start, please enter your kit number in the box below.',
+        'This number is located on the index card inside the bag of 3D-printed graphs.',
+        'If you do not have a bag of 3D-printed graphs, please check the "Online Participant" checkbox to verify that you are not an in-person participant.'),
+      
+      selectInput('kitNumber', 'Kit Number', choices = c('',1:21), selected = 'Oh hello'),
+      checkboxInput('onlineParticipant', 'Online Participant: Please only check this box if you do not have access to the 3D printed bar charts.', value = FALSE),
+      uiOutput('instructions')
+      
+
+)
 
 
-
+# In this survey, a series of graphs will be given to you. Each graph will have 10 bars; two of the bars will be identified with a circle and a triangle. First, we will ask you to identify which bar (circle or triangle) is smaller. Then, we will ask you to estimate the size of the smaller bar if the larger bar is 100 units tall.
+# 
+# In some cases, the graphs will be displayed on screen. In other cases, we will ask you to get one of the graphs from a ziploc bag you should have received before starting this study. For these 3D-printed graphs, we will ask you to tell us which graph you selected using the ID code engraved on the bottom of the chart.
+# 
+# The next screen will display a few sample graphs. The answers for each of these graphs are located underneath the sample.
 
 ##### UI Logic #####
 ui <- navbarPage(
@@ -75,9 +97,8 @@ ui <- navbarPage(
   id = 'navbar',
   tabPanel("Informed Consent", informed_consent),
   tabPanel("Demographics", demographic),
-  # header = ,
+  tabPanel("Instructions", instructions),
   collapsible = TRUE
-  
 )
 
 ##### Server Logic #####
@@ -135,6 +156,23 @@ server <- function(input, output, session) {
     validate(need(input$education != "", 'Please enter your education to continue'))
     validate(need(input$subjectZoo != "", 'Please enter your favorite zoo animal to continue'))
     actionButton("startPractice", "Start Practice")
+  })
+  
+  ##### Instructions Logic #####
+  
+  #Update kit value to 'Online' if online participant is checked
+  observeEvent(input$onlineParticipant, {
+    if(input$onlineParticipant == TRUE){
+      updateSelectInput(session, "kitNumber", choices = c('Online'))
+    } else {
+      updateSelectInput(session, "kitNumber", choices = c('',1:21),
+                        selected = '')
+    }
+  })
+  
+  output$instructions <- renderUI({
+    validate(need(input$kitNumber %in% c(1:21) | input$onlineParticipant == T, 'Please enter your kit number to continue'))
+    list(actionButton("startPractice", "Start Practice"))
   })
   
   
