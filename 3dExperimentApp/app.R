4e91111f1e577980e3dd201551f4872cb037947f
TWiedRW <61435221+TWiedRW@users.noreply.github.com>
2024-02-07 04:03:43 -0600

    Dev mode, working 3d digital plot

--- 3dExperimentApp/app.R
+++ 3dExperimentApp/app.R
@@ -2,12 +2,14 @@
 library(shiny)
 library(shinyjs)
 library(rgl)
+library(RSQLite)
 source('R/plot-code.R')
 graphData <- read.csv('Data/graphData.csv')
 printedKitInfo <- read.csv('Data/printedKitInfo.csv')
 practiceData <- read.csv('Data/practiceData.csv')
 source('R/create_lineup.R')
 source('R/generate_code.R')
+options(rgl.printRglwidget = TRUE)
 
 stl_files <- list.files('Plots/stl_files', pattern = '.stl', full.names = T)
 
@@ -142,6 +144,7 @@ experiment <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       uiOutput('expGraphTitle'),
+      uiOutput('expPrintedLabel'),
       radioButtons('expGraphs', 'Which marked bar is smaller?',
                    c('Circle', 'Triangle'), selected = ''),
       sliderInput('expEstimate', 'If the larger marked bar is 100 units tall, how tall is the smaller marked bar?',
@@ -150,7 +153,11 @@ experiment <- fluidPage(
       
     ),
     mainPanel(
-      uiOutput('expGraphDisplay')
+      uiOutput('expGraphDisplay'),
+      conditionalPanel(
+        condition = "input.subjectZoo == 'devmode'",
+        uiOutput('expDevStuff')
+      )
     )
   )
 )
@@ -239,6 +246,7 @@ server <- function(input, output, session) {
   
   output$informed_consent_button <- renderUI({
     validate(need(req(input$consent) != '', 'Please select an option to continue'))
+    
     list(actionButton("submitConsent", "Next page"))
 
   })
@@ -258,7 +266,16 @@ server <- function(input, output, session) {
     validate(need(input$gender != "", 'Please select your gender to continue'))
     validate(need(input$education != "", 'Please select your education to continue'))
     validate(need(input$subjectZoo != "", 'Please enter your favorite zoo animal to continue'))
-    actionButton("toInstructions", "Next Page")
+    
+    list(
+      conditionalPanel(
+        condition = "input.consent == 'FALSE'",
+        helpText('Demo Mode: Because you did not agree to participate in the experiment, your data will not be saved.')
+      ), 
+      actionButton("toInstructions", "Next Page")
+    )
+    
+    
   })
   
   observeEvent(input$toInstructions, {
@@ -411,26 +428,40 @@ server <- function(input, output, session) {
     list(h2(trial_data$graphTitle))
   })
   
+  output$expPrintedLabel <- renderUI({
+    
+    validate(need(trial_data$exp_plot_type == '3dPrint', ''))
+    selection_options <- trial_data$exp_lineup %>% 
+      filter(plot == '3dPrint')
+    
+    
+    if(trial_data$exp_plot_type == '3dPrint'){
+      list(selectInput('expPrintedLabel', 'Identify the label on the bottom of the 3D printed graph.',
+                       choices = c('-- Select ID --', sort(selection_options$kitMarking), 'Other')),
+           conditionalPanel(
+             'input.expPrintedLabel=="Other"',
+             textInput('expPrintedLabelCorrecter', 'If the identifier on the bottom of the plot does not match any of the available options, please enter the identifier here: ')
+           ))
+    }
+  })
   
   
   #Button to submit experiment estimate
   observeEvent(input$submitExp, {
+    
+    try(close3d())
 
-    # message(paste0('Participant completed experiment graph ',
-    #                (trial_data$exp_trial), ' (',
-    #                as.character((trial_data$exp_trial_data$plot))),  ')')
-    
-    # print(sprintf('Participant completed experiment graph %d (%s)', 
-    #               trial_data$exp_trial,
-    #               trial_data$exp_plot_type))
-    
-    # message('Participant completed an experiment graph')
+    message(sprintf('Participant completed experiment graph %d (%s)', 
+                    trial_data$exp_trial,
+                    trial_data$exp_plot_type))
     trial_data$exp_trial <- trial_data$exp_trial + 1
     updateSliderInput(session, "expEstimate", value = 50)
+    trial_data$exp_plot_type <- 'Refresh'
     if(trial_data$exp_trial > nrow(trial_data$exp_lineup)){
       updateNavbarPage(session, inputId = "navbar", "Finishing Up")
       trial_data$exp_trial <- NULL
     }
+    try(close3d())
   })
   
 
@@ -445,9 +476,10 @@ server <- function(input, output, session) {
   
   
   #Generate 3d digital plots
-  output$bar3dd <- renderPlot({
-    # validate(need(as.character(trial_data$exp_trial_data$plot) == '3dDigital', 'Error: Wrong plot type for this graph'))
+  output$bar3dd <- renderRglwidget({
     try(close3d())
+    validate(need(as.character(trial_data$exp_trial_data$plot) == '3dDigital', 'Error: Wrong plot type for this graph'))
+    open3d()
     Bar3D(stl_files[trial_data$exp_trial],
           color = 'blue')
     rglwidget()
@@ -460,8 +492,8 @@ server <- function(input, output, session) {
   #Generate 3d printed plots
   output$bar3dp <- renderPlot({
     validate(need(as.character(trial_data$exp_trial_data$plot) == '3dPrint', 'Error: Wrong plot type for this graph'))
-    Bar2D(trial_data$exp_trial_data,
-          shape_order = 1)
+    ggplot(mapping = aes(x = 0, y = 0)) + 
+      geom_label(label = 'Please select a 3D printed graph from your kit.')
   })
   
   
@@ -486,6 +518,7 @@ server <- function(input, output, session) {
              need(!is.null(trial_data$exp_lineup), 'Error: Trial lineup not created'),
              need(!is.null(trial_data$exp_trial), 'Error: Trial number not set'))
     
+    # try(close3d())
     
     #Subset lineup to current trial
     trial_data$exp_trial_data <- filter(trial_data$exp_lineup, 
@@ -493,23 +526,37 @@ server <- function(input, output, session) {
       left_join(graphData, by = 'fileID', relationship = 'many-to-many')
     
     #Lineup plot type
-    trial_data$exp_plot_type <- as.character(trial_data$exp_trial_data$plot)
-    
-    message(sprintf('Participant completed experiment graph %d', 
-                  # as.character(trial_data$sessionID), #this adds 10 messages for some weird reason
-                  trial_data$exp_trial))
-          
+    trial_data$exp_plot_type <- unique(as.character(trial_data$exp_trial_data$plot))
     
     #Render plot based on plot type
-    switch (as.character(unique(trial_data$exp_trial_data$plot)),
-            '2dDigital' = plotOutput('bar2d', width = '16.175cm', height = '9.5cm'),
+    list(switch (as.character(unique(trial_data$exp_trial_data$plot)),
+            '2dDigital' = plotOutput('bar2d', width = '14.175cm', height = '9.5cm'),
             '3dPrint' = plotOutput('bar3dp', width = '16.175cm', height = '9.5cm'),
             '3dStatic'  = plotOutput('bar3ds', width = '16.175cm', height = '9.5cm'),
             '3dDigital' = rglwidgetOutput('bar3dd')
-    )
+    ))
     
     
   })
+  
+  
+  output$devTrialData <- renderPrint({
+    print(list(
+      exp_kit = trial_data$exp_kit,             
+      exp_lineup = trial_data$exp_lineup,          
+      exp_trial = trial_data$exp_trial,           
+      exp_trial_data = trial_data$exp_trial_data,      
+      exp_plot_type = trial_data$exp_plot_type
+    ))
+  })
+  
+  output$expDevStuff <- renderUI({
+    list(
+      h2('Developer tools'),
+      verbatimTextOutput('devTrialData')
+    )
+  })
+  
   
   #-------------------------------------------------------------------------#
   
